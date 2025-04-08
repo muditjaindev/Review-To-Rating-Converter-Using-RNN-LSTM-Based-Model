5b6527b5be9867a5f24e7d8191406175c12b8890
Mudit <realcricket2021@gmail.com>
2025-04-08 10:58:49 +0530

    files added

---
+++ main.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+
+imdbData = imdb.load_data()
+print(imdbData)
